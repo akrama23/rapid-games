2c2555425faed78675f63d8ce1dfb74989dd46bb
akrama23 <arundelfootball23@gmail.com>
2021-08-02 21:40:00 -0400

    imported styled component and framer motion

---
+++ client/src/components/Game.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//styling and Animation 
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+
+const Game = () => {
+    
+    return(
+        <div>
+            <h3>Game Name</h3>
+            <p>Released Date</p>
+        </div>
+    );
+};
+export default Game;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
+//redex
 import { useDispatch } from 'react-redux';
 import { loadGames } from '../actions/gamesAction'
-
+//components
+import Game from '../components/Game'
+//styling and Animation 
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 
 const Home = () =>  {
